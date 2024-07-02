110dad9d5df336187bc240b6bcf5353bf7edf030
plazasoto <jarrettsguitarsfactory@gmail.com>
2024-07-02 16:57:15 -0400

    Alerta al presionar botón Enviar

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
     //inicialización requerida para tooltips
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+    $("#botonEnviar").click(function () {
+        alert("Mensaje enviado.");
+    });
+
 });
